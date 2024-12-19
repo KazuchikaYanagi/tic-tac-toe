30f3253531634fb9a998ea2d6c71df0bafc8777c
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-12-19 01:40:17 -0800

    update: insert deployed frontend url into development url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://tic-tac-toe-game-eta-gray.vercel.app",
     credentials: true,
   })
 );
