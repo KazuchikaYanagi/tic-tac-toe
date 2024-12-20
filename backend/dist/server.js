b414b9aa98c0713bc496fcf6744882668aaaff10
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-12-20 10:59:13 -0800

    remove: sameSite: none in cookiesession in server.ts

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -33,7 +33,7 @@ app.use((0, cookie_session_1.default)({
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: true,
-    sameSite: "none",
+    // sameSite: "none",
 }));
 // Router
 app.use("/api/users", user_routes_1.default);
