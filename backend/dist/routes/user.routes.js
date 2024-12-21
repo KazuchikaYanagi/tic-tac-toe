d66bc1d600988c56cc06d44d62751ba56a6cf313
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-12-21 01:05:19 -0800

    update: shift login route under play routes

--- backend/dist/routes/user.routes.js
+++ backend/dist/routes/user.routes.js
@@ -8,12 +8,12 @@ const user_controller_1 = __importDefault(require("../controllers/user.controlle
 const auth_1 = require("../middleware/auth");
 const userRouter = (0, express_1.Router)();
 userRouter.get("/play", auth_1.checkAuth, user_controller_1.default.userProfile);
+userRouter.post("/login", user_controller_1.default.loginUser);
 userRouter.get("/", auth_1.checkAuth, user_controller_1.default.getAllUsers);
 userRouter.get("/ranking", user_controller_1.default.getTopThreeUsers);
 userRouter.get("/logout", auth_1.checkAuth, user_controller_1.default.logoutUser);
 userRouter.get("/:id", auth_1.checkAuth, user_controller_1.default.getUserById);
 userRouter.post("/register", user_controller_1.default.registerUser);
-userRouter.post("/login", user_controller_1.default.loginUser);
 userRouter.put("/:id", auth_1.checkAuth, user_controller_1.default.updateUser);
 userRouter.delete("/:id", auth_1.checkAuth, user_controller_1.default.deleteUserById);
 exports.default = userRouter;
