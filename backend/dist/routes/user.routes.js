059df36905f6f952af320e84c426677b05e1d4a6
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-12-21 01:08:15 -0800

    add: checkAuth in login routes

--- backend/dist/routes/user.routes.js
+++ backend/dist/routes/user.routes.js
@@ -8,7 +8,7 @@ const user_controller_1 = __importDefault(require("../controllers/user.controlle
 const auth_1 = require("../middleware/auth");
 const userRouter = (0, express_1.Router)();
 userRouter.get("/play", auth_1.checkAuth, user_controller_1.default.userProfile);
-userRouter.post("/login", user_controller_1.default.loginUser);
+userRouter.post("/login", auth_1.checkAuth, user_controller_1.default.loginUser);
 userRouter.get("/", auth_1.checkAuth, user_controller_1.default.getAllUsers);
 userRouter.get("/ranking", user_controller_1.default.getTopThreeUsers);
 userRouter.get("/logout", auth_1.checkAuth, user_controller_1.default.logoutUser);
