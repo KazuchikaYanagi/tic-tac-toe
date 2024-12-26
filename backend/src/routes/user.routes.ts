3a290b9ec132519ebb5fc352d137d3d67d2f3be2
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-12-25 20:59:07 -0800

    update: change order routs

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,14 +4,14 @@ import { checkAuth } from "../middleware/auth";
 
 const userRouter = Router();
 
+userRouter.post("/register", userController.registerUser);
+userRouter.post("/login", userController.loginUser);
+
 userRouter.get("/play", checkAuth, userController.userProfile);
 userRouter.get("/", checkAuth, userController.getAllUsers);
 userRouter.get("/logout", checkAuth, userController.logoutUser);
 userRouter.get("/ranking", userController.getTopThreeUsers);
 userRouter.get("/:id", checkAuth, userController.getUserById);
-
-userRouter.post("/register", userController.registerUser);
-userRouter.post("/login", userController.loginUser);
 
 userRouter.put("/:id", checkAuth, userController.updateUser);
 userRouter.delete("/:id", checkAuth, userController.deleteUserById);
