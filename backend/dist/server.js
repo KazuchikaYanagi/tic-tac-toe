4af04911ec02ca881b29e13e5749d3a4d1221e5b
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-12-26 01:13:43 -0800

    update: frontend

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -32,7 +32,7 @@ app.use((0, cookie_session_1.default)({
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: "lax",
     httpOnly: true,
-    secure: true,
+    secure: false,
 }));
 // Router
 app.use("/api/users", user_routes_1.default);
