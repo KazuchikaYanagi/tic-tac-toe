6a27d4847dc0b4e418d254d1bb6d01fa5d9ea294
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-12-26 18:11:57 -0800

    update: cors origin in game.socket.ts

--- backend/dist/sockets/game.socket.js
+++ backend/dist/sockets/game.socket.js
@@ -16,7 +16,7 @@ const rooms = {};
 const setupGameSocket = (httpServer) => {
     const io = new socket_io_1.Server(httpServer, {
         cors: {
-            origin: "https://tic-tac-toe-7u0u.onrender.com",
+            origin: "https://tic-tac-toe-chi-pink.vercel.app",
             methods: ["GET", "POST"],
             credentials: true,
         },
